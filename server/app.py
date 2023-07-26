0c862dd10fa87f74272589d11ce837f4131580d9
Wesley Peters <wesleypeters@gmail.com>
2023-07-26 16:45:17 -0600

    define POST/PATCH/DELETE blocks

--- server/app.py
+++ server/app.py
@@ -66,5 +66,39 @@ def most_expensive_baked_good():
     )
     return response
 
+@app.route('/baked_goods', methods=['POST'])
+def create_baked_good():
+    data = request.form
+    new_baked_good = BakedGood(
+        name=data.get('name'),
+        price=data.get('price'),
+        bakery_id=data.get('bakery_id')
+    )
+    db.session.add(new_baked_good)
+    db.session.commit()
+    return make_response(new_baked_good.to_dict(), 201)
+
+@app.route('/bakeries/<int:id>', methods=['PATCH'])
+def update_bakery(id):
+    data = request.form
+    bakery = Bakery.query.filter_by(id=id).first()
+    if bakery:
+        bakery.name = data.get('name')
+        db.session.commit()
+        return make_response(bakery.to_dict(), 200)
+    else:
+        return make_response({"error": "Bakery not found"}, 404)
+
+@app.route('/baked_goods/<int:id>', methods=['DELETE'])
+def delete_baked_good(id):
+    baked_good = BakedGood.query.filter_by(id=id).first()
+    if baked_good:
+        db.session.delete(baked_good)
+        db.session.commit()
+        return make_response({"message": f"Deleted baked good with id {id}"}, 200)
+    else:
+        return make_response({"error": "Baked good not found"}, 404)
+
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
